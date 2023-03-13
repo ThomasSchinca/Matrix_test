bde29688c64c68d27b621fa433c4103b71819c37
ThomasSchinca <48996514+ThomasSchinca@users.noreply.github.com>
2023-03-13 18:53:53 +0000

    up

--- test_3.py
+++ test_3.py
@@ -18,9 +18,9 @@ from sklearn.preprocessing import MinMaxScaler
 import bisect
 from pmdarima.arima import auto_arima
 from dateutil.relativedelta import relativedelta
+from scipy.optimize import curve_fit
 
 ### Data Preparation 
-
 df = pd.read_csv("https://ucdp.uu.se/downloads/ged/ged221-csv.zip",
                  parse_dates=['date_start','date_end'],low_memory=False)
 df_tot = pd.DataFrame(columns=df.country.unique(),index=pd.date_range(df.date_start.min(),
@@ -74,6 +74,7 @@ for i in seq_n:
 # =============================================================================
 # General test       
 # =============================================================================
+'''
 res_tot=pd.DataFrame()
 for rad in [1,1.2,1.4,1.6,1.8,2,2.5]:        
     for win in [12,14,16,18,10,22,24,30,36]:
@@ -109,7 +110,7 @@ for rad in [1,1.2,1.4,1.6,1.8,2,2.5]:
                 tab_mot=pd.DataFrame(tab_mot)
                 m_tab = tab_mot.mean(axis=1)
                 std_tab = tab_mot.std(axis=1)
-                '''
+                
                 plt.figure(figsize=(8,6))
                 for i in range(len(tab_mot.columns)):
                     plt.plot(tab_mot.iloc[0:win,i],color='blue',alpha=0.1)
@@ -122,7 +123,7 @@ for rad in [1,1.2,1.4,1.6,1.8,2,2.5]:
                 plt.title('Motifs '+str(mot+1)+' (CI +/- Std) - N = '+str(len(comb)))
                 #plt.savefig('Figures_motifs_mean_std/Global Motif '+str(mot+1)+'.png')
                 plt.show()
-                '''
+                
                 #### ARIMA forecast comparison 
                 res=pd.DataFrame()
                 for i in ind_m:
@@ -151,7 +152,7 @@ for rad in [1,1.2,1.4,1.6,1.8,2,2.5]:
                         pred_mp =  m_tab.iloc[-h_max:]*(df_tot_m.loc[:index,col].iloc[-win:].max()/m_tab.iloc[:-h_max].max())
                         pred_mp.index= pred_ar.index
                         obs = df_tot_m.loc[:index_obs,col].iloc[-h_max+win:]
-                        '''
+                        
                         plt.figure(figsize=(12,8))
                         plt.plot(obs,label='Observed value',marker='o')
                         plt.plot(pred_mp,label='MP predict',marker='o')
@@ -160,14 +161,14 @@ for rad in [1,1.2,1.4,1.6,1.8,2,2.5]:
                         plt.legend()
                         plt.title(col)
                         plt.show()
-                        '''
+                        
                         df_res=pd.DataFrame([obs.iloc[-h_max:].reset_index(drop=True),pred_ar.reset_index(drop=True),pred_mp.reset_index(drop=True)]).T
                         df_res.columns = ['obs','ar','mp']
                         res=pd.concat([res,df_res],axis=0)
                         
                     except:
                         1#print('This sequence is not doable : '+str(col)+'  '+str(index))
-                '''
+                
                 plt.figure(figsize=(12,8))        
                 for i in range(h_max):
                     k=res.loc[i,:]
@@ -175,7 +176,7 @@ for rad in [1,1.2,1.4,1.6,1.8,2,2.5]:
                 plt.xticks([*range(h_max)],[*range(1,h_max+1)])
                 plt.hlines(0, -0.25, h_max-0.75, colors='red',linestyles='--')
                 plt.title('Motifs '+str(mot+1))
-                '''
+                
                 res_l=[]
                 for i in range(h_max):
                     k=res.loc[i,:]
@@ -188,33 +189,41 @@ for rad in [1,1.2,1.4,1.6,1.8,2,2.5]:
                 res_tot=pd.concat([res_tot,res])
             except:
                 1
-            
-plt.scatter(res_tot.n_seq,res_tot.iloc[:,:5].mean(axis=1))
+'''
+
+res_tot = pd.read_csv('res_matrix.csv',index_col=0)
+
+def func(x, a,b,c):
+    return a * np.exp(-b * x) + c
+popt, pcov = curve_fit(func, res_tot.n_mot+1, res_tot.iloc[:,:5].mean(axis=1))
+
+plt.scatter(res_tot.n_mot+1,res_tot.iloc[:,:5].mean(axis=1))
+plt.plot(range(0,21), func(range(0,21), *popt),'g-',)
 plt.ylim(-500,800)
 plt.xlim(0,20)
 plt.hlines(0,0,20,color='red')
 plt.ylabel('MAE forecasting improv')
-plt.xlabel('Number of seq in motif')
+plt.xlabel('Number of other seq in motif')
 plt.show()
 
-good = res_tot[(res_tot.H1>0) & (res_tot.H2>0) & (res_tot.H3>0) & (res_tot.H4>0) & (res_tot.H5>0) & (res_tot.n_seq>=12)]
+good = res_tot[(res_tot.H1>0) & (res_tot.H2>0) & (res_tot.H3>0) & (res_tot.H4>0) & (res_tot.H5>0) & (res_tot.n_mot>=12)]
 good.index = range(len(good))
 good_top= good.loc[good.iloc[:,:5].mean(axis=1).sort_values().iloc[-5:].index]
-
 
 # =============================================================================
 # The best motifs 
 # =============================================================================
 
-win_sel = [16,18,18]
-mot_sel = [13,16,17]
-rad_sel = [2,2,2]
+### Small nice shape
+win_sel = [36,18]
+mot_sel = [14,17]
+rad_sel = [1.6,2]
 
 for row in range(len(win_sel)):
     win=int(win_sel[row])
     h_max=5
     prof = mp.compute(n_test,windows=[win,200])
-    profile = mp.discover.motifs(prof,k=20,max_neighbors=100,radius=int(rad_sel[row]))
+    profile = mp.discover.motifs(prof,k=20,max_neighbors=100,radius=rad_sel[row])
     pmp= profile['pmp'][0,:]
     to=0
     exclude=[]
@@ -222,7 +231,7 @@ for row in range(len(win_sel)):
         to=to+len(i)
         exclude=exclude+[*range(to-win,to)]
     
-    mot = int(mot_sel[row])
+    mot = int(mot_sel[row])-1
     motifs = profile['motifs'][mot]['motifs']
     neigh = profile['motifs'][mot]['neighbors']
     comb=motifs+neigh
@@ -238,10 +247,10 @@ for row in range(len(win_sel)):
     plt.title('Motif '+str(row+1)+' - Matrix Profile')
     plt.show()
     
-    tab_mot = n_test[ind_m[0]:ind_m[0]+h_max+win].reshape((h_max+win,1))
+    tab_mot = n_test[ind_m[0]:ind_m[0]+win].reshape((win,1))
     for i in ind_m[1:]:
         try:
-            tab_mot=np.concatenate([tab_mot,n_test[i:i+h_max+win].reshape((h_max+win,1))],axis=1)
+            tab_mot=np.concatenate([tab_mot,n_test[i:i+win].reshape((win,1))],axis=1)
         except:
             1
     scaler = MinMaxScaler()
@@ -253,15 +262,10 @@ for row in range(len(win_sel)):
     
     plt.figure(figsize=(8,6))
     for i in range(len(tab_mot.columns)):
-        plt.plot(tab_mot.iloc[0:win,i],color='blue',alpha=0.1)
-        plt.plot(tab_mot.iloc[win-1:h_max+win,i],color='red',alpha=0.1)
-    plt.plot(m_tab.iloc[0:win],color='blue',linewidth=2)    
-    plt.plot(m_tab.iloc[win-1:h_max+win],color='red',linewidth=2) 
-    plt.fill_between([*range(win)],m_tab.iloc[0:win]-std_tab.iloc[0:win],m_tab.iloc[0:win]+std_tab.iloc[0:win],color='blue',alpha=0.2)    
-    plt.plot(m_tab.iloc[win-1:h_max+win],color='red') 
-    plt.fill_between([*range(win-1,h_max+win)],m_tab.iloc[win-1:h_max+win]-std_tab.iloc[win-1:h_max+win],m_tab.iloc[win-1:h_max+win]+std_tab.iloc[win-1:h_max+win],color='red',alpha=0.2) 
+        plt.plot(tab_mot.iloc[:,i],color='blue',alpha=0.1)
+    plt.plot(m_tab.iloc[:],color='blue',linewidth=2)    
+    plt.fill_between([*range(win)],m_tab-std_tab,m_tab+std_tab,color='blue',alpha=0.2)      
     plt.title('Motifs '+str(row+1)+' (CI +/- Std) - '+str(win)+' months window')
-    #plt.savefig('Figures_motifs_mean_std/Global Motif '+str(mot+1)+'.png')
     plt.show()
     
     #### ARIMA forecast comparison 
@@ -300,13 +304,21 @@ for row in range(len(win_sel)):
             pred_mp_p=pd.concat([obs.iloc[-h_max-1:-h_max],pred_mp])
             pred_ar_p=pd.concat([obs.iloc[-h_max-1:-h_max],pred_ar])
             
+            if ((len(ind_m)%3==0) & (len(ind_m)//3==1)) | (len(ind_m)//3==0) :
+                ax[c_l].plot(obs,label='Observed value',marker='o')
+                ax[c_l].plot(pred_mp_p,label='MP predict',marker='o')
+                ax[c_l].plot(pred_ar_p,label='AR predict',marker='o')
+                ax[c_l].vlines(obs.index[win-1],0,obs.max()+0.1*obs.max(),colors='red',linestyles='--')
+                ax[c_l].legend()
+                ax[c_l].set_title(col)
+            else:
+                ax[c_r,c_l].plot(obs,label='Observed value',marker='o')
+                ax[c_r,c_l].plot(pred_mp_p,label='MP predict',marker='o')
+                ax[c_r,c_l].plot(pred_ar_p,label='AR predict',marker='o')
+                ax[c_r,c_l].vlines(obs.index[win-1],0,obs.max()+0.1*obs.max(),colors='red',linestyles='--')
+                ax[c_r,c_l].legend()
+                ax[c_r,c_l].set_title(col)
             
-            ax[c_r,c_l].plot(obs,label='Observed value',marker='o')
-            ax[c_r,c_l].plot(pred_mp_p,label='MP predict',marker='o')
-            ax[c_r,c_l].plot(pred_ar_p,label='AR predict',marker='o')
-            ax[c_r,c_l].vlines(obs.index[win-1],0,obs.max()+0.1*obs.max(),colors='red',linestyles='--')
-            ax[c_r,c_l].legend()
-            ax[c_r,c_l].set_title(col)
             
             df_res=pd.DataFrame([obs.iloc[-h_max:].reset_index(drop=True),pred_ar.reset_index(drop=True),pred_mp.reset_index(drop=True)]).T
             df_res.columns = ['obs','ar','mp']
@@ -342,15 +354,23 @@ for row in range(len(win_sel)):
                 pred_mp_p=pd.concat([obs.iloc[-h_max-1:-h_max],pred_mp])
                 pred_ar_p=pd.concat([obs.iloc[-h_max-1:-h_max],pred_ar])
                 
-                ax[c_r,c_l].plot(obs,label='Observed value',marker='o')     
-                ax[c_r,c_l].plot(pred_mp_p,label='MP predict',marker='o')
-                ax[c_r,c_l].plot(pred_ar_p,label='AR predict',marker='o')
-                ax[c_r,c_l].vlines(obs.index[win],0,obs.max()+0.1*obs.max(),colors='red',linestyles='--')
-                ax[c_r,c_l].legend()
-                ax[c_r,c_l].set_title(col)
-                
-                df_res=pd.DataFrame([obs.iloc[-h_max:].reset_index(drop=True),pred_ar.reset_index(drop=True),pred_mp.reset_index(drop=True)]).T
-                df_res.columns = ['obs','ar','mp']
+                if (len(ind_m)%3==0) & (len(ind_m)//3==1) | (len(ind_m)//3==0) :
+                    ax[c_l].plot(obs,label='Observed value',marker='o')
+                    ax[c_l].plot(pred_mp_p,label='MP predict',marker='o')
+                    ax[c_l].plot(pred_ar_p,label='AR predict',marker='o')
+                    ax[c_l].vlines(obs.index[win-1],0,obs.max()+0.1*obs.max(),colors='red',linestyles='--')
+                    ax[c_l].legend()
+                    ax[c_l].set_title(col)
+                else:
+                    ax[c_r,c_l].plot(obs,label='Observed value',marker='o')
+                    ax[c_r,c_l].plot(pred_mp_p,label='MP predict',marker='o')
+                    ax[c_r,c_l].plot(pred_ar_p,label='AR predict',marker='o')
+                    ax[c_r,c_l].vlines(obs.index[win-1],0,obs.max()+0.1*obs.max(),colors='red',linestyles='--')
+                    ax[c_r,c_l].legend()
+                    ax[c_r,c_l].set_title(col)
+                    
+                    df_res=pd.DataFrame([obs.iloc[-h_max:].reset_index(drop=True),pred_ar.reset_index(drop=True),pred_mp.reset_index(drop=True)]).T
+                    df_res.columns = ['obs','ar','mp']
                 res=pd.concat([res,df_res],axis=0)
             else: 
                 print('Overlaping seq')
@@ -367,3 +387,25 @@ for row in range(len(win_sel)):
     plt.xticks([*range(h_max)],[*range(1,h_max+1)])
     plt.hlines(0, -0.25, h_max-0.75, colors='red',linestyles='--')
     plt.title('Motifs '+str(row+1)+' with N = '+str(len(k)))
+    plt.show()
+ 
+thres = res_tot[res_tot.n_mot>1]
+thres.index = range(len(thres))   
+ 
+plt.scatter(thres.n_mot+1,thres.iloc[:,:5].mean(axis=1))
+plt.plot(range(2,21), func(range(2,21), *popt),'g-',)
+plt.ylim(-500,800)
+plt.xlim(0,20)
+plt.hlines(0,0,20,color='red')
+plt.ylabel('MAE forecasting improv')
+plt.xlabel('Number of other seq in motif')
+plt.show()
+
+for i in thres.rad.unique():
+    df_t= thres[thres.rad==i]
+    plt.plot(df_t.iloc[:,:5].mean(axis=0),label=str(i),marker='o',linewidth=0)
+plt.hlines(0,-0.5,4.5,linestyles='--',colors='red')
+plt.legend()
+plt.show()
+
+    
